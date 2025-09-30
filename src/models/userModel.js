b813c86f75a57e6c60d6c8ed5b5ac3b8737fa4f4
SidharthGithubb <sahoo.sidharth96@gmail.com>
2025-10-01 00:15:39 +0530

    User registration api created

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,22 +2,51 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        minlength: 4,
+        maxlength: 30,
+        required: [true, "First name is required"]
     },
     lastName: {
-        type: String
+        type: String,
+        minlength: 4,
+        maxlength: 30,
     },
     emailId: {
-        type: String
+        type: String,
+        lowercase: true,
+        trim: true,
+        required: [true, "Email ID is required"],
+        unique: true
     },
     password: {
-        type: String
+        type: String,
+        minlength: 6,
+        maxlength: 15,
+        required: [true, "Password is required"]
     },
     age: {
-        type: Number
+        type: Number,
+        maxlength:3
     },
     gender: {
-        type: String
+        type: String,
+        // This method will validate the condition only when a new record/object is created.
+        validate(value){
+            if(!["male", "female", "other"].includes(value.toLowerCase())){
+                throw new Error("Gender data is not valid");
+            }
+        }
+    },
+    bio: {
+        type: String,
+        default: "",
+        maxlength: 250
+    },
+    skill: {
+        type: String,
+        enum: ["JavaScript", "Python", "Java", "C++", "Ruby", "Go", "PHP", "C#", "TypeScript", "Swift", "Kotlin", "Rust", "Dart", "Scala", "Perl", "Haskell", "Lua", "Elixir", "Clojure", "Erlang"],
+        default: "JavaScript"
     }
 }, {
     timestamps: true
