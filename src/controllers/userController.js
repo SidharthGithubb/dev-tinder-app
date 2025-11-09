c8776718cd32994ea597bed52ec8a9b36e8c70ac
SidharthGithubb <sahoo.sidharth96@gmail.com>
2025-11-09 19:58:17 +0530

    pagination added to the feed api

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,4 @@
+const { isEmpty } = require("validator");
 const ConnectionRequest = require("../models/connectionRequestModel");
 const User = require("../models/userModel");
 //DESC Get all the pending connection requests for logged in user
@@ -10,21 +11,22 @@ const getAllRequestsForUser = async (req, res) => {
     const requests = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", ["firstName", "lastName", "emailId", "profileImage"]);
+    }).populate("fromUserId", [
+      "firstName",
+      "lastName",
+      "emailId",
+      "profileImage",
+    ]);
 
-    res
-      .status(200)
-      .json({
-        message: "Pending requests fetched successfully",
-        data: requests,
-      });
+    res.status(200).json({
+      message: "Pending requests fetched successfully",
+      data: requests,
+    });
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        message: "error while fetching connection requests for user",
-        error: error.message,
-      });
+    res.status(500).json({
+      message: "error while fetching connection requests for user",
+      error: error.message,
+    });
   }
 };
 
@@ -32,36 +34,47 @@ const getAllRequestsForUser = async (req, res) => {
 //METHOD: GET
 //ROUTE: /api/user/connections
 const getAllConnectionsForUser = async (req, res) => {
-    try {
-        const loggedInUser = req.user; //From token
+  try {
+    const loggedInUser = req.user; //From token
 
-        const allConnections = await ConnectionRequest.find({
-            $or: [
-                {fromUserId : loggedInUser._id, status: "accepted"},
-                {toUserId : loggedInUser._id, status: "accepted"},
-            ]
-        }).populate("fromUserId", ["firstName", "lastName", "emailId", "profileImage"])
-        .populate("toUserId", ["firstName", "lastName", "emailId", "profileImage"]);
-        
-        const connections = allConnections.map((row) => {
-            if(row.fromUserId._id.equals(loggedInUser._id)) {
-                return row.toUserId;
-            } else {
-                return row.fromUserId;
-            }
-        })
-        
-        res.status(200).json({
-            message: "Connections fetched successfully",
-            data: connections,
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: "error while fetching connections for user",
-            error: error.message,
-        });
-    }
-}
+    const allConnections = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "accepted" },
+        { toUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", [
+        "firstName",
+        "lastName",
+        "emailId",
+        "profileImage",
+      ])
+      .populate("toUserId", [
+        "firstName",
+        "lastName",
+        "emailId",
+        "profileImage",
+      ]);
+
+    const connections = allConnections.map((row) => {
+      if (row.fromUserId._id.equals(loggedInUser._id)) {
+        return row.toUserId;
+      } else {
+        return row.fromUserId;
+      }
+    });
+
+    res.status(200).json({
+      message: "Connections fetched successfully",
+      data: connections,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "error while fetching connections for user",
+      error: error.message,
+    });
+  }
+};
 
 //DESC Get feed for logged in user
 //METHOD: GET
@@ -70,11 +83,15 @@ const feedController = async (req, res) => {
   try {
     const loggedInUser = req.user;
 
+    const page = req.query.page ? parseInt(req.query.page) : 1;
+    let limit = req.query.limit ? parseInt(req.query.limit) : 10;
+    limit = limit > 50 ? 50 : limit; //Max limit 50
+
+    const skip = (page - 1) * limit;
     //Find all connection requests (sent + received) for logged in user
     const connectionsRequests = await ConnectionRequest.find({
-      $or: [{fromUserId: loggedInUser._id},
-        {toUserId: loggedInUser._id}]
-    }).select("fromUserId toUserId status")
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId status");
 
     const hideUsersFromFeed = new Set();
     connectionsRequests.forEach((connection) => {
@@ -84,21 +101,38 @@ const feedController = async (req, res) => {
 
     //Also hide logged in user
     hideUsersFromFeed.add(loggedInUser._id.toString());
-    
+
     //Find all users except the ones in hideUsersFromFeed
     const newConnections = await User.find({
-      _id: { $nin: Array.from(hideUsersFromFeed) }
-    }).select("firstName lastName emailId profileImage headline bio gender skill");
-
-    res.status(200).json({
-      message: "Feed fetched successfully",
-      data: newConnections,
-    });
+      _id: { $nin: Array.from(hideUsersFromFeed) },
+    })
+      .select(
+        "firstName lastName emailId profileImage headline bio gender skill"
+      )
+      .skip(skip)
+      .limit(limit);
+      
+    if (!newConnections) {
+      throw new Error("Error in fetching new connections");
+    } else if (newConnections.length === 0) {
+      res.status(200).json({
+        message: "No new connections available",
+      });
+    } else {
+      res.status(200).json({
+        message: "Feed fetched successfully",
+        data: newConnections,
+      });
+    }
   } catch (error) {
     res.status(500).json({
       message: "error while fetching feed for user",
       error: error.message,
     });
   }
-}
-module.exports = { getAllRequestsForUser, getAllConnectionsForUser, feedController };
+};
+module.exports = {
+  getAllRequestsForUser,
+  getAllConnectionsForUser,
+  feedController,
+};
